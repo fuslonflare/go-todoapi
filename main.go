8cacd109a41478303e8d3b13c6f756844e1e6e25
fuslonflare <phuwarin.21@gmail.com>
2024-01-31 10:05:43 +0700

    Cleaning up Routers.

--- main.go
+++ main.go
@@ -10,7 +10,6 @@ import (
 	"syscall"
 	"time"
 
-	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 	"github.com/joho/godotenv"
 	"golang.org/x/time/rate"
@@ -49,22 +48,11 @@ func main() {
 		log.Println("auto migrate db", err)
 	}
 
-	r := gin.Default()
-	config := cors.DefaultConfig()
-	config.AllowOrigins = []string{
-		"http://localhost:8080",
-	}
-	config.AllowHeaders = []string{
-		"Origin",
-		"Authorization",
-		"TransactionID",
-	}
-	r.Use(cors.New(config))
-
+	r := router.NewMyRouter()
 	gormStore := store.NewGormStore(db)
 	handler := todo.NewTodoHandler(gormStore)
 
-	r.POST("/todos", router.NewGinHandler(handler.NewTask))
+	r.POST("/todos", handler.NewTask)
 
 	ctx, stop := signal.NotifyContext(context.Background(), syscall.SIGINT, syscall.SIGTERM)
 	defer stop()
--- router/gin.go
+++ router/gin.go
@@ -1,6 +1,7 @@
 package router
 
 import (
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 	"github.com/pallat/todoapi/todo"
 )
@@ -38,4 +39,28 @@ func NewGinHandler(handler func(todo.Context)) gin.HandlerFunc {
 	return func(ctx *gin.Context) {
 		handler(NewMyContext(ctx))
 	}
+}
+
+type MyRouter struct {
+	*gin.Engine
+}
+
+func NewMyRouter() *MyRouter {
+	r := gin.Default()
+	config := cors.DefaultConfig()
+	config.AllowOrigins = []string{
+		"http://localhost:8080",
+	}
+	config.AllowHeaders = []string{
+		"Origin",
+		"Authorization",
+		"TransactionID",
+	}
+	r.Use(cors.New(config))
+
+	return &MyRouter{r}
+}
+
+func (r *MyRouter) POST(path string, handler func(todo.Context)) {
+	r.Engine.POST(path, NewGinHandler(handler))
 }
